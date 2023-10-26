580c6be08cdacc0ca03a3e45cc7d10b557e27b56
yarrobong <oneboardsholi@gmail.com>
2023-10-26 13:26:07 +0500

    Добавьте файлы проекта.

---
+++ NoDigit/Program.cs
@@ -0,0 +1,20 @@
+﻿string str = Console.ReadLine();
+string result = String.Empty;
+string pattern = "0123456789";
+
+foreach (char item in str)
+{
+    if (char.IsDigit(item))
+    {
+        pattern = pattern.Replace(item.ToString(), "");
+    }
+}
+
+if (pattern.Length == 0)
+{
+    Console.WriteLine("NO");
+}
+else
+{
+    Console.WriteLine(pattern);
+}
